6c95437345b2f0d7c2be1f681812a2522e133abf
okunnig <okunnig@noreply>
2019-07-03 18:38:14 +0200

    Add stupid vec3 implementation

--- src/main.rs
+++ src/main.rs
@@ -25,10 +25,87 @@ impl Bitmap {
     }
 }
 
+#[derive(Copy, Clone)]
+struct Vec3 {
+    x: f32,
+    y: f32,
+    z: f32,
+}
+
+impl Vec3 {
+    fn new(x: f32, y: f32, z: f32) -> Vec3 {
+        Vec3 { x, y, z }
+    }
+
+    fn x(self) -> f32 {
+        self.x
+    }
+
+    fn y(self) -> f32 {
+        self.y
+    }
+
+    fn z(self) -> f32 {
+        self.z
+    }
+
+    fn add(self, other: Vec3) -> Vec3 {
+        Vec3 { x: self.x + other.x, y: self.y + other.y, z: self.z + other.z }
+    }
+
+    fn subtract(self, other: Vec3) -> Vec3 {
+        Vec3 { x: self.x - other.x, y: self.y - other.y, z: self.z - other.z }
+    }
+
+    fn negate(self) -> Vec3 {
+        Vec3 { x: -self.x, y: -self.y, z: -self.z }
+    }
+
+    fn multiply(self, other: Vec3) -> Vec3 {
+        Vec3 { x: self.x * other.x, y: self.y * other.y, z: self.z * other.z }
+    }
+
+    fn multiply_scalar(self, scalar: f32) -> Vec3 {
+        Vec3 { x: self.x * scalar, y: self.y * scalar, z: self.z * scalar }
+    }
+
+    fn div(self, other: Vec3) -> Vec3 {
+        Vec3 { x: self.x / other.x, y: self.y / other.y, z: self.z / other.z }
+    }
+
+    fn div_scalar(self, scalar: f32) -> Vec3 {
+        Vec3 { x: self.x / scalar, y: self.y / scalar, z: self.z / scalar }
+    }
+
+    fn dot(self, other: Vec3) -> f32 {
+        self.x * other.x + self.y * other.y + self.z * other.z
+    }
+
+    fn cross(self, other: Vec3) -> Vec3 {
+        Vec3 {
+            x: self.y * other.z - self.z * other.y,
+            y: -(self.x * other.z - self.z * other.x),
+            z: self.x * other.y - self.y * other.x
+        }
+    }
+
+    fn length(self) -> f32 {
+        (self.x * self.x + self.y * self.y + self.z * self.z).sqrt()
+    }
+
+    fn squared_length(self) -> f32 {
+        self.x * self.x + self.y * self.y + self.z * self.z
+    }
+
+    fn unit_vector(self) -> Vec3 {
+        self.div_scalar(self.length())
+    }
+}
+
 fn main() -> Result<(), Box<Error>> {
     let width = 800;
     let height = 600;
-    let mut window = Window::new("Raytracer",width, height, WindowOptions::default())?;
+    let mut window = Window::new("Raytracer", width, height, WindowOptions::default())?;
     window.set_cursor_style(CursorStyle::ClosedHand);
 
     let mut bitmap = Bitmap::new(width, height);
